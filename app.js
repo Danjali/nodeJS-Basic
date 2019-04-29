10432b442b7319dfbcd303c4dc4988efdfc8ee87
Anjali Deosarkar <anjali.deosarkar@ad.infosys.com>
2019-04-29 17:29:43 +0530

    adds addProject API

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// import express from 'express';
-// const express = require('express');
-// const app = express();
-// app.get('/', (req, res) => res.send('Hello World!'));
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-
-// app.get("/url", (req, res, next) => {
-//     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-// })
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -18,7 +8,3 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
-
-// const server = app.listen(3000, function () {
-//     console.log("app running on port.", server.address().port);
-// });
--- routes/routes.js
+++ routes/routes.js
@@ -1,30 +1,31 @@
-var faker = require("faker");
-var appRouter = function (app) {
+var cors = require("cors");
+const express = require("express");
+const port = process.env.PORT || 5000;
+const appRouter = function (app) {
+  
   app.get("/", function(req, res) {
     res.status(200).send("Welcome to our restful API");
   });
 
-  app.post("/projects", function(req, res) {
-    const data = ({
-      description: "",
-      key: "tempo_project",
-      name: "tempo_project"
-      });
-    res.status(200).send(data);
-  });
+  // app.post("/projects", function(req, res) {
+  //   const data = ({
+  //     description: "",
+  //     key: "tempo_project",
+  //     name: "tempo_project"
+  //     });
+  //   res.status(200).send(data);
+  // });
 
-  const port = process.env.PORT || 5000;
-  // app.use(bodyParser.json());
-  // app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.get('/api/addProject', (req, res) => {
+  app.use(cors());
+  app.use(express.json());
+  app.get('/api/projects', (req, res) => {
     res.send({ express: 'Hello From Express' });
-  });
+  }); 
 
   app.post('/api/addProject', (req, res) => {
     console.log(req.body);
     res.send(
-      `I received your POST request. This is what you sent me: ${req.body.post}`,
+      `I received your POST request. This is what you sent me: ${req.body.key} ${req.body.description} ${req.body.name}`,
     );
   });
 
