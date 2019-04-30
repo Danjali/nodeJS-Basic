b289500c1a3bc88f5f706627bc917197a542af2c
Anjali Deosarkar <anjali.deosarkar@ad.infosys.com>
2019-04-30 17:19:16 +0530

    adds bitbucket api

--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,52 @@
-var cors = require("cors");
+const cors = require("cors");
 const express = require("express");
+const Request = require("request");
 const port = process.env.PORT || 5000;
 const appRouter = function (app) {
-  
+const { createBitbucketAPI } = require('bitbucket-api-v2');
+const bitbucketApi = createBitbucketAPI();
+
+  app.use(cors());
+  app.use(express.json());
+
   app.get("/", function(req, res) {
     res.status(200).send("Welcome to our restful API");
   });
 
-  // app.post("/projects", function(req, res) {
-  //   const data = ({
-  //     description: "",
-  //     key: "tempo_project",
-  //     name: "tempo_project"
-  //     });
-  //   res.status(200).send(data);
-  // });
-
-  app.use(cors());
-  app.use(express.json());
   app.get('/api/projects', (req, res) => {
     res.send({ express: 'Hello From Express' });
   }); 
 
   app.post('/api/addProject', (req, res) => {
-    console.log(req.body);
+    let received = postProjectData(req.body);
     res.send(
-      `I received your POST request. This is what you sent me: ${req.body.key} ${req.body.description} ${req.body.name}`,
+      `Received data: ${received}`,
     );
   });
+
+  postProjectData = (projectData) => {
+    const requestPayload = projectData;
+    const base64 = require('base-64'),
+      username = 't.m.mahajan@gmail.com',
+      password = 'Tej.12345';
+      let headers={
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + base64.encode(username + ':' + password)
+    };
+    Request.post({
+      "headers": headers ,
+      "url": "https://api.bitbucket.org/2.0/teams/shared_teamtest1/projects/",
+      "body": JSON.stringify(requestPayload),
+  }, (error, response, body) => {
+      if(error) {
+          return console.dir(error);
+      }
+      if(response){
+        return response;
+      }
+      console.dir(JSON.parse(body));
+  });
+  }
 
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
